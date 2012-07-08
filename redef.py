ed2f5f7d1f650125153217d6e2c91f8ce87c523c
Joe Heyming <joeheyming@gmail.com>
2012-07-08 15:45:34 -0700

    Redef is here!

---
+++ redef.py
@@ -0,0 +1,120 @@
+'''
+redef.py
+@author joeheyming@gmail.com
+Test module that redefines attributes of a module or class
+When the test goes out of scope, your redefined attribute goes back to normal behavior.
+'''
+
+import types
+import inspect
+
+class CallableWrapper:
+    '''Captures information on a redefined function'''
+    called = 0
+    method_args = None
+    named_method_args = None
+
+    def capture(self, args, kwargs):
+        '''Store the input to the captured function'''
+        self.called = self.called + 1
+        self.method_args = args
+        self.named_method_args = kwargs
+
+    def reset(self):
+        '''Set the wrapper to a base state where the function was never called'''
+        self.called = 0
+        self.method_args = None
+        self.named_method_args = None
+
+    def __init__(self, rd):
+        '''Take a Redef object and wrap the function you want to redefine'''
+        # don't keep any references to the Redef object around
+        # or else the __del__ function will not work correctly
+        is_class_method = not inspect.ismethod(rd.old_value)
+        func = rd.value
+
+        def tocall(*args, **kwargs):
+            self.capture(args, kwargs)
+
+            # pop off the redef class variable to keep the
+            # faked out function signature the same
+            if is_class_method:
+                args = args[1:]
+
+            return func(*args, **kwargs)
+
+        # @staticmethods need to return a function bound
+        #  to the class
+        if is_class_method:
+            tocall = types.MethodType(tocall, rd.obj)
+        self.wrapped = tocall
+
+class Redef(object):
+    '''An object that when deleted puts the redefined object back to normal'''
+
+    def __init__(self, obj, key, value):
+        '''Make sure you keep the returned Redef object in a variable so that the __del__ function is not called immediately
+
+Good: >>> rd_somefunc = Redef(SomeClass, "attr", lambda s, x: "something else")
+Bad: >>> Redef(SomeClass, "attr", lambda s, x: "something else")
+
+        '''
+        self.key = key
+        self.obj = obj
+        self.old_value = getattr(self.obj, self.key)
+        self.value = value
+
+        if value is None:
+            self.value = self.old_value
+        self.wrapper = CallableWrapper(self)
+        if callable(self.value):
+            setattr(self.obj, self.key, self.wrapper.wrapped)
+        else:
+            setattr(self.obj, self.key, self.value)
+
+    def __del__(self):
+        '''Can be called explicitly or implied when gone out of scope'''
+        setattr(self.obj, self.key, self.old_value)
+
+    def called(self):
+        '''ask the wrapper how many times the redef has been called'''
+        return self.wrapper.called
+
+    def method_args(self):
+        '''ask the wrapper for the most recent non-named args'''
+        return self.wrapper.method_args
+
+    def named_method_args(self):
+        '''ask the wrapper for the most recent named args'''
+        return self.wrapper.named_method_args
+
+    def reset(self):
+        '''ask the wrapper to forget all wrapped information'''
+        self.wrapper.reset()
+
+def redef(obj, key, value):
+    '''A static constructor helper function'''
+    return Redef(obj, key, value)
+
+class WriteCapturer:
+    def __init__(self, func, args):
+        self.output = ''
+        self.func = func
+        self.args = args
+    def capture(self):
+        self.returned = self.func(self.args)
+    def write(self, *args):
+        self.output = self.output + ' '.join([str(x) for x in args])
+
+def capture_output_(func, args, output_type):
+    import sys
+    writer = WriteCapturer(func, args)
+    rd_write = redef(sys, output_type, writer)
+    writer.capture()
+    return writer
+
+def stdout_of(func, args):
+    return capture_output_(func, args, 'stdout')
+
+def stderr_of(func, args):
+    return capture_output_(func, args, 'stderr')
---
+++ test_redef.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+import unittest
+from redef import redef, stdout_of
+from oktest import ok, test
+
+class Somewhere:
+    def show(self, message):
+        return self.__class__.__name__ + ', ' + message
+
+class RedefTest(unittest.TestCase):
+    @test("Test1: redef del test")
+    def _(self):
+        somewhere = Somewhere()
+        ok(somewhere.show('beyond the sea')) == 'Somewhere, beyond the sea'
+        rd_show = redef(Somewhere, 'show', lambda self, message: 'hi')
+        ok(somewhere.show('over the rainbow')) == 'hi'
+        ok(rd_show.method_args()) == (somewhere, 'over the rainbow')
+        ok(rd_show.named_method_args()) == {}
+        ok(rd_show.called()) == 1
+        rd_show.reset()
+        ok(rd_show.method_args()) == None
+        ok(rd_show.named_method_args()) == None
+        ok(rd_show.called()) == 0
+        ok(somewhere.show('beyond the stars')) == 'hi'
+        del rd_show
+        ok(somewhere.show('beyond the stars')) == 'Somewhere, beyond the stars'
+
+    @test("Test2: test 3 won't have bye after test 2")
+    def _(self):
+        somewhere = Somewhere()
+        ok(somewhere.show('beyond the stars')) == 'Somewhere, beyond the stars'
+        rd_show = redef(Somewhere, 'show', lambda self, message: 'bye')
+        ok(somewhere.show('over the rainbow')) == 'bye'
+    @test("Test3: redef doesn't effect this block")
+    def _(self):
+        somewhere = Somewhere()
+        ok(somewhere.show('beyond the stars')) == 'Somewhere, beyond the stars'
+
+    @test('Test4: redef class variable')
+    def _(self):
+        class Math:
+            pi = 3.14
+        m = Math()
+        ok(m.pi) == 3.14
+        rd_p = redef(m, 'pi', 3)
+        ok(m.pi) == 3
+        ok(rd_p.called()) == 0
+        ok(rd_p.method_args()) == None
+        ok(rd_p.named_method_args()) == None
+        del rd_p
+        ok(m.pi) == 3.14
+
+    @test('Test5: redef static method')
+    def _(self):
+        class Hello:
+            @staticmethod
+            def world(x):
+                return 'Hello %s!' % x
+        ok(Hello.world('joe')) == 'Hello joe!'
+        rd_w = redef(Hello, 'world', lambda x: 'Goodbye %s' % x)
+
+        ok(Hello.world('joe')) == 'Goodbye joe'
+        ok(rd_w.method_args()) == (Hello, 'joe')
+
+    @test('Test6: class inheritance')
+    def _(self):
+        class A:
+            '''Base class'''
+            def a(self, x):
+                return x * 2
+        class B(A):
+            def a(self, x):
+                ret = A.a(self, x)
+                return 'b' + ret
+        bee = B()
+        ok(bee.a('z')) == 'bzz'
+
+        want = 'baa baa'
+        rd_b = redef(B, 'a', lambda s,x: want)
+        ok(bee.a('z')) == 'baa baa'
+        ok(rd_b.called()) == 1
+        del rd_b
+
+        want = 'atter batter'
+        rd_a = redef(A, 'a', lambda s,x: want)
+        ok(bee.a('z')) == 'batter batter'
+
+        ok(rd_a.called()) == 1
+
+    @test('Test7: multiple calls, capture stdout')
+    def _(self):
+        class Pirate:
+            drunk = 0
+            def attack(self):
+                print 'arrr!'
+
+            def hoard(self, booty):
+                print 'yarrr, don\'t touch me %s' % booty
+
+            def plunder(self, booty):
+                self.attack()
+                for x in range(1, 10):
+                    self.drink()
+                self.hoard(booty)
+                return 'punch'
+
+            def drink(self):
+                print 'burp!'
+                self.drunk = self.drunk + 1
+
+        blackbeard = Pirate()
+        rd_d = redef(Pirate, 'drink', None)
+        captured = stdout_of(blackbeard.plunder, 'buried treasure')
+
+        ok(rd_d.called()) == 9
+
+        talk = 'arrr!\n' + 'burp!\n' * 9 + 'yarrr, don\'t touch me buried treasure\n'
+
+        ok(captured.output) == talk
+        ok(captured.returned) == 'punch'
+
+        captured = stdout_of(blackbeard.plunder, 'parrot')
+        ok(rd_d.called()) == 18
+
+        rd_d.reset()
+        ok(rd_d.called()) == 0
+
+if __name__ == '__main__':
+    unittest.main()
